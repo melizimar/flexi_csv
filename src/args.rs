fa8165909f898986aa99f9302b8e42950ad0adce
melizimar <melizimar@gmail.com>
2024-10-17 16:58:41 -0300

    adicionando argumentos

--- src/args.rs
+++ src/args.rs
@@ -20,17 +20,31 @@ pub enum OperationsTypes {
 
 #[derive(Debug, Args)]
 pub struct SliceCommand{
-    #[clap(subcommand)]
-    pub command: SliceSubcommand,
-}
-
-#[derive(Debug, Subcommand)]
-pub enum SliceSubcommand{
-    Split(SplitCSV)
-}
-
-#[derive(Debug, Args)]
-pub struct SplitCSV {
-    pub input_path: PathBuf,
-    pub output_dir: PathBuf
+    /// Caminho para o arquivo de entrada (obrigatório)
+    #[arg(short, long)]
+    input_file: PathBuf,
+    /// Caminho para o diretório de saída (obrigatório)
+    #[arg(short, long)]
+    output_dir: PathBuf,
+    /// Número de linhas para cada arquivo de saída (obrigatório)
+    #[arg(short, long)]
+    num_lines_output_file: usize,
+    /// Delimitador do arquivo CSV o padrão é ";"
+    #[arg(short, long, default_value_t = ';')]
+    delimiter: char,
+    /// Número de Threads para criação dos arquivos. O valor padrão é definido de acordo com cada maquina
+    #[arg(long, default_value_t = num_cpus::get())]
+    num_threads: usize,
+    /// Recebe o nome dos campos como argumento e transforma eles em UPPERCASE
+    #[arg(long, num_args = 1..)]
+    to_uppercase: Option<Vec<String>>,
+    /// Recebe o nome dos campos como argumento e transforma eles em LOWERCASE
+    #[arg(long, num_args = 1..)]
+    to_lowercase: Option<Vec<String>>,
+    /// Recebe o nome dos campos como argumento e transforma eles em NORMALIZED (sem acentuação)
+    #[arg(long, num_args = 1..)]
+    to_normalized: Option<Vec<String>>,
+    /// Recebe o nome dos campos como argumento e transforma as informações em TITLE CASE
+    #[arg(long, num_args = 1..)]
+    to_titlecase: Option<Vec<String>>,
 }
