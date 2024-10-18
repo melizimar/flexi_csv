f0e9af6cc6377e27f4bec666ce37d77abe206c86
melizimar <melizimar@gmail.com>
2024-10-18 13:45:14 -0300

    Adicionada a funcionalidade de divide o arquivo em pedaços menores

--- src/args.rs
+++ src/args.rs
@@ -3,14 +3,14 @@ use crate::threads;
 use clap::{Args, Parser, Subcommand};
 use std::path::PathBuf;
 
-#[derive(Debug, Parser)]
+#[derive(Debug, Clone, Parser)]
 #[clap(author, version, about)]
 pub struct FlexiCsvArgs {
     #[clap(subcommand)]
     pub operations_types: OperationsTypes,
 }
 
-#[derive(Debug, Subcommand)]
+#[derive(Debug, Clone, Subcommand)]
 pub enum OperationsTypes {
     /// Slice => Divide o arquivo CSV em novos arquivos menores
     Slice(SliceCommand),
@@ -19,58 +19,58 @@ pub enum OperationsTypes {
     Transform(TransformCommand),
 }
 
-#[derive(Debug, Args)]
+#[derive(Debug, Clone, Args)]
 pub struct SliceCommand {
     /// Caminho para o arquivo de entrada (obrigatório)
     //#[arg(short, long)] -> Remover comentario
-    input_file: PathBuf,
+    pub input_file: PathBuf,
     /// Caminho para o diretório de saída (obrigatório)
     //#[arg(short, long)] -> Remover comentario
-    output_dir: PathBuf,
+    pub output_dir: PathBuf,
     /// Número de linhas para cada arquivo de saída (obrigatório)
     //#[arg(short, long)]
-    num_lines_output_file: usize,
+    pub num_lines_output_file: usize,
     /// Delimitador do arquivo CSV o padrão é ";"
     #[arg(short, long, default_value_t = ';')]
-    delimiter: char,
+    pub delimiter: char,
     /// Número de Threads para criação dos arquivos. O valor padrão é definido de acordo com cada maquina
     #[arg(long, default_value_t = threads::get_num_threads())]
-    num_threads: usize,
+    pub num_threads: usize,
     /// Recebe o nome dos campos como argumento e transforma eles em UPPERCASE
     #[arg(long, num_args = 1..)]
-    to_uppercase: Option<Vec<String>>,
+    pub to_uppercase: Option<Vec<String>>,
     /// Recebe o nome dos campos como argumento e transforma eles em LOWERCASE
     #[arg(long, num_args = 1..)]
-    to_lowercase: Option<Vec<String>>,
+    pub to_lowercase: Option<Vec<String>>,
     /// Recebe o nome dos campos como argumento e transforma eles em NORMALIZED (sem acentuação)
     #[arg(long, num_args = 1..)]
-    to_normalized: Option<Vec<String>>,
+    pub to_normalized: Option<Vec<String>>,
     /// Recebe o nome dos campos como argumento e transforma as informações em TITLE CASE
     #[arg(long, num_args = 1..)]
-    to_titlecase: Option<Vec<String>>,
+    pub to_titlecase: Option<Vec<String>>,
 }
 
-#[derive(Debug, Args)]
+#[derive(Debug, Clone, Args)]
 pub struct TransformCommand {
     /// Caminho para o arquivo de entrada (obrigatório)
     //#[arg(short, long)]
-    input_file: PathBuf,
+    pub input_file: PathBuf,
     /// Delimitador do arquivo CSV o padrão é ";"
     #[arg(short, long, default_value_t = ';')]
-    delimiter: char,
+    pub delimiter: char,
     /// Número de Threads para criação dos arquivos. O valor padrão é definido de acordo com cada maquina
     #[arg(long, default_value_t = 6)]
-    num_threads: usize,
+    pub num_threads: usize,
     /// Recebe o nome dos campos como argumento e transforma eles em UPPERCASE
     #[arg(long, num_args = 1..)]
-    to_uppercase: Option<Vec<String>>,
+    pub to_uppercase: Option<Vec<String>>,
     /// Recebe o nome dos campos como argumento e transforma eles em LOWERCASE
     #[arg(long, num_args = 1..)]
-    to_lowercase: Option<Vec<String>>,
+    pub to_lowercase: Option<Vec<String>>,
     /// Recebe o nome dos campos como argumento e transforma eles em NORMALIZED (sem acentuação)
     #[arg(long, num_args = 1..)]
-    to_normalized: Option<Vec<String>>,
+    pub to_normalized: Option<Vec<String>>,
     /// Recebe o nome dos campos como argumento e transforma as informações em TITLE CASE
     #[arg(long, num_args = 1..)]
-    to_titlecase: Option<Vec<String>>,
+    pub to_titlecase: Option<Vec<String>>,
 }
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,16 @@ mod csv_utils;
 mod threads;
 
 use clap::Parser;
+use csv_utils::{count_csv_lines, get_number_csv_files};
 
 use deunicode::deunicode;
 use polars::prelude::*;
-use std::fmt::Arguments;
-use std::path::{Path, PathBuf};
 use std::collections::HashMap;
 use std::error::Error;
+use std::fmt::Arguments;
 use std::fs::{create_dir_all, File};
 use std::io::{self, BufRead};
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::{process, thread};
@@ -19,18 +20,206 @@ use std::{process, thread};
 use indicatif::{ProgressBar, ProgressStyle};
 use inflector::cases::titlecase::to_title_case;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let arguments = args::FlexiCsvArgs::parse();
 
-    if let args::OperationsTypes::Transform(cmd) = &arguments.operations_types {
-        // Fluxo para TransformCommand
-        println!("Executando TransformCommand no arquivo {:#?}", cmd);
-    } 
+    if let args::OperationsTypes::Slice(args) = arguments.operations_types.clone() {
+        // Abre o arquivo
+        let metadata = std::fs::metadata(args.input_file.clone())?;
 
-    if let args::OperationsTypes::Slice(cmd) = &arguments.operations_types {
-        // Fluxo para SliceCommand
-        println!("Executando SliceCommand no arquivo {:#?}", cmd);
+        // Verifica se o arquivo existe
+        if !args.input_file.exists() {
+            println!("O arquivo não existe, por gentileza informe um arquivo valido.");
+            process::exit(1);
+        } else if metadata.len() == 0 {
+            println!("O arquivo está vazio, por gentileza informe um arquivo valido.");
+            process::exit(1);
+        }
+
+        // Cria o diretório de saída se não existir
+        create_dir_all(args.output_dir.clone())?;
+
+        let input_file = args.input_file.clone();
+
+        // let mut transformations: HashMap<String, Vec<String>> = HashMap::new();
+
+        // if let Some(vec) = &args.to_uppercase {
+        //     transformations.insert("to_uppercase".to_string(), vec.clone());
+        // }
+        // if let Some(vec) = &args.to_lowercase {
+        //     transformations.insert("to_lowercase".to_string(), vec.clone());
+        // }
+        // if let Some(vec) = &args.to_normalized {
+        //     transformations.insert("to_normalized".to_string(), vec.clone());
+        // }
+        // if let Some(vec) = &args.to_titlecase {
+        //     transformations.insert("to_titlecase".to_string(), vec.clone());
+        // }
+
+        let num_lines_input_file = count_csv_lines(&input_file).unwrap();
+        let chunck_size = args.num_lines_output_file * 20;
+        let num_csv_files = get_number_csv_files(
+            num_lines_input_file as f64,
+            args.num_lines_output_file as f64,
+        )
+        .unwrap();
+
+        let dataframes = CsvChunkReader::new(&args.input_file, chunck_size); // 100.000 linhas por chunk
+
+        let indexes_file_names: Vec<usize> = (1..1 + num_csv_files).collect();
+
+        let shared_indexes = Arc::new(Mutex::new(indexes_file_names));
+
+        // Criar barra de progresso
+        let progress_bar = ProgressBar::new(num_csv_files as u64);
+        progress_bar.set_style(
+        ProgressStyle::default_bar()
+            .template(
+                "Processando arquivos...{pos}/{len}\n[{elapsed_precise}] [{wide_bar}] ({percent}%) ",
+            )
+            .unwrap(),
+    );
+
+        for mut df in dataframes {
+            // for (key, columns) in transformations.clone() {
+            //     if key == *"to_uppercase" {
+            //         for column in &columns {
+            //             let uppercase_column = df
+            //                 .lazy() // Usa lazy execution
+            //                 .with_column(col(column).str().to_uppercase())
+            //                 .collect()?;
+            //             df = uppercase_column;
+            //         }
+            //     }
+            //     if key == *"to_lowercase" {
+            //         for column in &columns {
+            //             let lowercase_column = df
+            //                 .lazy() // Usa lazy execution
+            //                 .with_column(col(column).str().to_lowercase())
+            //                 .collect()?;
+            //             df = lowercase_column;
+            //         }
+            //     }
+            //     if key == *"to_normalized" {
+            //         for column in &columns {
+            //             let col_series = df.column(column)?.str()?;
+            //             // Remova acentos de cada valor na série
+
+            //             let no_accents: Vec<Option<String>> = col_series
+            //                 .into_iter()
+            //                 .map(|opt_s| opt_s.map(deunicode)) // Remove acentos
+            //                 .collect();
+
+            //             // Cria uma nova série com os valores sem acentos
+            //             let new_series = Series::new(column.into(), no_accents);
+
+            //             // Substitui a coluna antiga pela nova no DataFrame
+            //             df.replace(column, new_series)?;
+            //         }
+            //     }
+            //     if key == *"to_titlecase" {
+            //         for column in &columns {
+            //             let col_series = df.column(column)?.str()?;
+
+            //             // Remova acentos de cada valor na série
+            //             let no_accents: Vec<Option<String>> = col_series
+            //                 .into_iter()
+            //                 .map(|opt_s| opt_s.map(to_title_case)) // Remove acentos
+            //                 .collect();
+
+            //             // Cria uma nova série com os valores sem acentos
+            //             let new_series = Series::new(column.into(), no_accents);
+
+            //             // Substitui a coluna antiga pela nova no DataFrame
+            //             df.replace(column, new_series)?;
+            //         }
+            //     }
+            // }
+
+            // Número de linhas por arquivo
+            let chunk_size = args.num_lines_output_file;
+
+            // Total de arquivos que vamos gerar
+            let total_chunks = get_number_csv_files(df.height() as f64, chunk_size as f64).unwrap();
+
+            // Um vetor para armazenar as threads
+            let mut handles = vec![];
+
+            // Usamos Arc e Mutex para compartilhar o DataFrame entre as threads
+            let df = Arc::new(df);
+
+            // Criar as threads
+            for i in 0..total_chunks {
+                // Clonar o Arc DF para cada thread tenha acesso ao mesmo DF
+                let df = Arc::clone(&df);
+
+                // Clonar o Arc para que cada thread tenha acesso ao mesmo vetor
+                let shared_indexes = Arc::clone(&shared_indexes);
+
+                // Caminho para o diretorio de output
+                let output_dir: String = args.output_dir.clone().to_str().unwrap().to_string();
+
+                // Obtém o nome do arquivo sem a extensão
+                let file_name = if let Some(file_stem) = input_file.file_stem() {
+                    file_stem.to_str().unwrap_or("").to_string() // Converte para String
+                } else {
+                    String::new() // Retorna uma String vazia se não conseguir
+                };
+
+                let handle = thread::spawn(move || {
+                    let start = i * chunk_size;
+                    let end = ((i + 1) * chunk_size).min(df.height());
+                    let mut chunk = df.slice(start as i64, end - start);
+
+                    let index_value = {
+                        let mut data = shared_indexes.lock().unwrap();
+
+                        // Verifica se há elementos no vetor
+                        if data.is_empty() {
+                            None // Retorna None se o vetor estiver vazio
+                        } else {
+                            // Remove e retorna o primeiro valor
+                            Some(data.remove(0))
+                        }
+                    };
+
+                    // Criar o nome do arquivo
+                    let output_file_name = format!(
+                        "{}/{}-{}.csv",
+                        &output_dir,
+                        file_name.clone(),
+                        index_value.unwrap()
+                    );
+
+                    let mut file = File::create(&output_file_name).unwrap();
+
+                    // Gravar o DataFrame no arquivo
+                    CsvWriter::new(&mut file)
+                        .include_header(true)
+                        .with_separator(args.delimiter as u8)
+                        .finish(&mut chunk)
+                        .unwrap();
+                });
+
+                handles.push(handle);
+            }
+
+            // Aguardar a conclusão de todas as threads
+            for handle in handles {
+                let _ = handle.join();
+                progress_bar.inc(1);
+            }
+        }
+
+        progress_bar.finish_with_message("Todos os arquivos foram processados.");
     }
+
+    if let args::OperationsTypes::Transform(args) = arguments.operations_types.clone() {
+        // Fluxo para TransformCommand
+        println!("Executando TransformCommand no arquivo {:#?}", args);
+    }
+
+    Ok(())
 }
 
 struct CsvChunkReader<'a> {
